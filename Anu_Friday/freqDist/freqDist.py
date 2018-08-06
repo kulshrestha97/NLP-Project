8a238dd234995de4c09636c2d8f56c006ddae3aa
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2018-08-07 01:11:50 +0530

    did part q1 of nltk

--- Anu_Friday/freqDist/freqDist.py
+++ Anu_Friday/freqDist/freqDist.py
@@ -1,15 +1,11 @@
-from nltk.corpus import reuters
+from nltk.corpus import reuters, brown
 from nltk.tokenize import TreebankWordTokenizer
 import nltk
 
-#words = TreebankWordTokenizer().tokenize(reuters)
-print(reuters.categories())
 
-# use conditional freq dist to fint 2 letter, 3 letter frequency
-# Find longest words
-# segment chinese, japanese data
-
-
-cfd = nltk.ConditionalFreqDist((genre, word) for genre in reuters.categories(
-) for word in reuters.words(categories=genre))
-cfd.tabulate()
+print("\nExploring Brown Corpus\n~~~~~~~~~~~~~~~~~~~~\n")
+print("Categories : ")
+print(brown.categories())
+print("\nChoosing category \"News\": \n")
+print("\nText: ")
+print(" ".join(brown.words(categories="news")))
